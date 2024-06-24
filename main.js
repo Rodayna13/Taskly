73c255b2fa95a8a2fb5b063d88601d80b9fc2dbe
Rodayna Yasser <104641669+Rodayna13@users.noreply.github.com>
2024-06-25 01:36:57 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+document.getElementById("loginbtn").addEventListener("click", function(event) {
+    event.preventDefault(); 
+
+    const emailInput = document.getElementById("email-input");
+    const emailValue = emailInput.value;
+    const passInput = document.getElementById("pass");
+    const passValue = passInput.value ;
+    let email = localStorage.getItem('email')
+    let password = localStorage.getItem('password')
+    
+    if (email!=emailValue) {
+        emailInput.style.borderColor = 'red';
+        document.getElementById("alert").style.display = "block";
+        document.getElementById("alert").innerHTML = "Invalid E-mail";
+
+    }
+
+    if (password!=passValue) {
+        passInput.style.borderColor = 'red';
+        document.getElementById("alert").style.display = "block";
+        document.getElementById("alert").innerHTML = "Invalid Password";
+
+
+
+    }
+    
+  
+
+    if (emailValue == email && passValue == password) {
+        location.href="/taskly/index.html";
+    }
+else {
+            let alert = document.getElementById("alert")
+            alert.classList.remove('d-none')
+            alert.classList.remove('d-block')
+}});
+
+  
+
+    
---
+++ main2.js
@@ -0,0 +1,36 @@
+document.getElementById("signUpbtn").addEventListener("click", function(event) {
+   
+    event.preventDefault(); 
+
+    const emailInput = document.getElementById("email-input");
+    const emailValue = emailInput.value;
+    const fullNameInput = document.getElementById("name-input");
+    const fullNameValue = fullNameInput.value;
+    const fullNameRegX = /^[A-Za-z]+[A-Za-z]+[A-Za-z]+$/;
+    const emailRegX = /^[a-zA-Z0-9]{3,}@(gmail|yahoo|outlook)\.com$/;
+    const passRegx = /^.{4,}$/;
+    const passInput = document.getElementById("pass");
+    const passValue = passInput.value;
+    
+    function storeData() {
+        localStorage.setItem('fullname', fullNameValue);
+        localStorage.setItem('email', emailValue);
+        localStorage.setItem('password', passValue);
+    }
+
+    if (!emailRegX.test(emailValue) || !fullNameRegX.test(fullNameValue) || !passRegx.test(passValue)) {
+        if (!emailRegX.test(emailValue)) {
+            emailInput.style.borderColor = 'red';
+        }
+        if (!fullNameRegX.test(fullNameValue)) {
+            fullNameInput.style.borderColor = 'red';
+        }
+        if (!passRegx.test(passValue)) {
+            passInput.style.borderColor = 'red';
+        }
+    } else {
+        storeData();
+        window.location.href = "Sign_in.html";
+    }
+    
+});
